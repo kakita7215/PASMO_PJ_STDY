6b95a3e2f2af491408b15ca4acb0bb653c5b014d
kakita7215 <kakita7215@gmail.com>
2025-12-25 10:39:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
 const path = require('path');
+
 const app = express();
  
 app.use(express.static(path.join(__dirname)));
  
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(`✅ Server is running on port ${PORT}`);
+
 });
+ 
