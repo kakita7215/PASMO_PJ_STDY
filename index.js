59cc5d204bc5b39c56311c709a66372a303808b8
kakita7215 <kakita7215@gmail.com>
2025-12-25 10:57:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const http = require('http');
+
+const fs = require('fs');
 
 const path = require('path');
-
-const app = express();
- 
-app.use(express.static(path.join(__dirname)));
  
 const PORT = process.env.PORT || 3000;
+ 
+const server = http.createServer((req, res) => {
 
-app.listen(PORT, () => {
+  let filePath = path.join(__dirname, 'index.html');
+ 
+  fs.readFile(filePath, (err, content) => {
 
-  console.log(`✅ Server is running on port ${PORT}`);
+    if (err) {
+
+      res.writeHead(500);
+
+      res.end('Error loading file');
+
+    } else {
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+
+      res.end(content, 'utf-8');
+
+    }
+
+  });
+
+});
+ 
+server.listen(PORT, () => {
+
+  console.log(`✅ Server running on port ${PORT}`);
 
 });
  
