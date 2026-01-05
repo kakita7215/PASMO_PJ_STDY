aceb2116e88459b61c281a92209201e1232f7de9
kakita7215 <kakita7215@gmail.com>
2026-01-05 14:00:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ wss.on("connection", (ws) => {
       espSocket.send(
         JSON.stringify({
           type: "text",
-          text: data.text
+          text: data.text ,
+          speed:200
         })
       );
     }
