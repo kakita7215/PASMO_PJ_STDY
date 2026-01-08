999afc4f15591f60bb33be2c440e6091a051dc6b
kakita7215 <kakita7215@gmail.com>
2026-01-08 11:38:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-
 let ws = new WebSocket("ws://esp32.local/ws");
 
 function sendAlarm() {
   const time = document.getElementById("alarmTime").value;
   const enable = document.getElementById("alarmEnable").checked;
 
-
   ws.send(JSON.stringify({
     alarm: time,
     enable: enable
   }));
-
-  if (data.type === "alarm" && espSocket) {
-    espSocket.send(JSON.stringify(data));
-  }
-
+  
 }
