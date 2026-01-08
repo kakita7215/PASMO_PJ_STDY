311d7720959a81dbd2e1fd7237e7005bdde7d2fc
kakita7215 <kakita7215@gmail.com>
2026-01-08 12:08:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ function sendAlarm() {
   const time = document.getElementById("alarmTime").value;
   const enable = document.getElementById("alarmEnable").checked;
 
+  if (!time) return;
+
+  const [hour, minute] = time.split(":").map(Number);
+
   ws.send(JSON.stringify({
-    alarm: time,
+    type: "alarm",
+    hour: hour,
+    minute: minute,
     enable: enable
   }));
 }
